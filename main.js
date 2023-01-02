87a0c45bb29cae25171ed038ead2b4ea518df176
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-01-02 16:20:42 +0200

    Add js file

---
+++ main.js
@@ -0,0 +1,15 @@
+function mobileMenu() {
+    const nonDisplay = document.getElementById('mobile-menu')
+    const blur = document.getElementById('blur')
+   
+    blur.classList.toggle("blur")
+    nonDisplay.classList.toggle('non-display')
+   }
+   
+   function removeBlur() {
+    const nonDisplay = document.getElementById('mobile-menu')
+    const blur = document.getElementById('blur')
+   
+    blur.classList.toggle("blur")
+    nonDisplay.classList.toggle('non-display')
+   }
