eca484ca0d9d3bfa916fcef0a4057b64e0fd2cbc
Naji-97 <elbairabennaji@gmail.com>
2023-01-03 09:56:08 +0100

    edit the font weight, spaces, and font-size of the  nav items

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function mobileMenu() {
     const nonDisplay = document.getElementById('mobile-menu')
     const blur = document.getElementById('blur')
+    const projects = document.getElementById('projects')
+    const about = document.getElementById('about')
+    const contact = document.getElementById('contact')
+
    
     blur.classList.toggle("blur")
     nonDisplay.classList.toggle('non-display')
+    projects.classList.toggle("blur")
+    about.classList.toggle("blur")
+    contact.classList.toggle("blur")
    }
    
-   function removeBlur() {
-    const nonDisplay = document.getElementById('mobile-menu')
-    const blur = document.getElementById('blur')
-   
-    blur.classList.toggle("blur")
-    nonDisplay.classList.toggle('non-display')
-   }
